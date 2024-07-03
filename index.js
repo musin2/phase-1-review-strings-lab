a0a52bf5963ca871e3d4aea87e41b0717f96b677
musin2 <muhsindaud75@gmail.com>
2024-07-04 00:04:45 +0300

    Adds lab content

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ const currentUser = 'Grace Hopper';
 
 let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 let excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
-let shortGreeting = "Welcome, " + currentUser.charAt(0) + "!";
+let shortGreeting = "Welcome, " + currentUser.slice(0,1) + "!";
